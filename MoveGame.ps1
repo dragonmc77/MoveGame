e45ffc0bc93960cb9f111df0f9225bd190e190f1
dragonmc77 <dragonmc77@gmail.com>
2020-09-14 19:06:46 -0400

    First version
    First working version.

--- MoveGame.ps1
+++ MoveGame.ps1
@@ -1,30 +1,41 @@
-param (
-    [string]$Link,
-    [string]$Target
-)
-<#  import the mklink functionality from the shell #>
-Add-Type @"
-using System;
-using System.Runtime.InteropServices;
+function global:moveGame() {
+    $selection = $PlayniteApi.MainView.SelectedGames
+    <#  check that only one game is selected #>
+    if ($selection.Count -gt 1) {
+        $PlayniteApi.Dialogs.ShowErrorMessage("Only one game can be moved at a time.","Please select only one game")
+        break
+    }
+    $selectedGame = $selection[0]
+    $installDir = $selectedGame.InstallDirectory
 
-namespace mklink
-{
-    public class symlink
+    <#  import the mklink functionality from the shell #>
+    Add-Type @"
+    using System;
+    using System.Runtime.InteropServices;
+
+    namespace mklink
     {
-        [DllImport("kernel32.dll")]
-        public static extern bool CreateSymbolicLink(string lpSymlinkFileName, string lpTargetFileName, int dwFlags);
+        public class symlink
+        {
+            [DllImport("kernel32.dll")]
+            public static extern bool CreateSymbolicLink(string lpSymlinkFileName, string lpTargetFileName, int dwFlags);
+        }
+    }
+"@
+    if (Test-Path -Path $installDir) {
+        $newDir = $PlayniteApi.Dialogs.SelectFolder()
+        if (-not $newDir) {break}
+        Move-Item -Path $installDir -Destination $newDir -Force -ErrorAction SilentlyContinue -ErrorVariable err
+        if ($err) {$PlayniteApi.Dialogs.ShowErrorMessage("Most likely cause is Playnite not running as admin","Error Moving Folder"); break} 
+    }
+
+    if (-not $installDir.EndsWith("\")) {$installDir = $installDir + "\"}
+    if (-not $newDir.EndsWith("\")) {$newDir = $newDir + "\"}
+    $success = [mklink.symlink]::CreateSymbolicLink($installDir,$newDir,3)
+    if ($success) {
+        $response = $PlayniteApi.Dialogs.ShowMessage("The game folder was moved and a link was created.","Success")
+    }
+    else {
+        $response = $PlayniteApi.Dialogs.ShowErrorMessage("Failed creating link.","Failed")
     }
 }
-"@
-if (Test-Path -Path $Link) {
-    Write-Host "Path specified as link already exists. Moving folder..." -ForegroundColor Yellow
-    Move-Item -Path $Link -Destination $Target -Force -ErrorAction SilentlyContinue -ErrorVariable err
-    if (-not $err) {Write-Host "Folder moved successfully." -ForegroundColor Green} 
-        else {Write-Error "Could not move folder."; break}
-}
-
-if (-not $Link.EndsWith("\")) {$Link = $Link + "\"}
-if (-not $Target.EndsWith("\")) {$Target = $Target + "\"}
-$success = [mklink.symlink]::CreateSymbolicLink($Link,$Target,3)
-if ($success) {Write-Host "Symbolic link created successfully" -ForegroundColor Green}
-    else {Write-Error "Could not create symbolic link."}
